f2ed377ed3504fcdb1c938afb200c42ffae5145a
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-07-10 09:27:44 +0800

    1、Kotlin 重写 equals

--- src/kotlinTest/EqualsHashCode.kt
+++ src/kotlinTest/EqualsHashCode.kt
@@ -2,6 +2,11 @@ package kotlinTest
 
 class Client(val name: String, val postalCode: Int) {
     override fun toString(): String = "Client(name=$name,postalCode=$postalCode)"
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Client)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
 }
 
 fun main(args: Array<String>) {
