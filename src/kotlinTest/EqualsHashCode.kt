df2f1bff2074c25b560fe0cbab864b4930f6c254
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-07-10 09:39:31 +0800

    1、Kotlin 重写 hashCode

--- src/kotlinTest/EqualsHashCode.kt
+++ src/kotlinTest/EqualsHashCode.kt
@@ -3,10 +3,12 @@ package kotlinTest
 class Client(val name: String, val postalCode: Int) {
     override fun toString(): String = "Client(name=$name,postalCode=$postalCode)"
     override fun equals(other: Any?): Boolean { // Any 类似于 java.lang.Object 它是Kotlin中所有类的超类
-        if (other == null || other !is Client) //检查other变量是否为Client
+        if (other == null || other !is Client) //检查other变量是否为Client  Kotlin中的is是Java的instanceOf的对应物
             return false
         return name == other.name && postalCode == other.postalCode //检查相应的属性是否相等
     }
+
+    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
 }
 
 fun main(args: Array<String>) {
@@ -14,4 +16,7 @@ fun main(args: Array<String>) {
     val client2 = Client("OuyangPeng", 1000)
     println(client1)
     println(client1 == client2)
+
+    val processed = setOf<Client>(Client("Alice", 343562))
+    println(processed.contains(Client("Alice", 343562)))
 }
