b7bef6c8071ec244d0c6df3a8fc129bdc8eb322c
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-17 21:06:17 +0800

    1、Kotlin 单行注释和多行注释

---
+++ src/kotlinTest/comment/comment.kt
@@ -0,0 +1,16 @@
+package kotlinTest.comment
+
+/*
+ 这里面的内容全部是多行注释
+ */
+fun main(args:Array<String>){
+    //这是一行简单的注释
+    println("Hello World!")
+//    println("这行代码被注释了，将不会被编译、执行！")
+
+    //Kotlin 多行注释可以嵌套
+
+    /* 这是第一个多行注释的开头
+    /* 这是第二个被嵌套的多行注释*/
+    这是第一个多行注释的结尾*/
+}
