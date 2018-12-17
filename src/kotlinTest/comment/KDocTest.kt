aeb63bca605b81152764b6a4a23287f012a80d68
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-17 21:09:04 +0800

    1、Kotlin 文档注释

---
+++ src/kotlinTest/comment/KDocTest.kt
@@ -0,0 +1,18 @@
+package kotlinTest.comment
+
+/**
+ * 测试文档注释
+ * @author OuyangPeng
+ */
+public class KDocTest {
+
+    /**
+     * 简单的加法
+     * @param a 第一个加数
+     * @param b 第二个加数
+     * @return 两个数的和
+     */
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
