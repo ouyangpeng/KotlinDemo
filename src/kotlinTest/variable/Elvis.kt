076f427eb726728d4b9bf36b6a0669916f96096f
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 14:44:58 +0800

    1、Kotlin elvis运算符

---
+++ src/kotlinTest/variable/Elvis.kt
@@ -0,0 +1,15 @@
+package kotlinTest.variable
+
+fun main(args: Array<String>) {
+
+    var b: String? = "oyp"
+    var len1 = if (b != null) b.length else -1
+    println(len1)
+
+    b = null
+    //使用 Elvis运算符
+    //若第一个运算数不为null，运算结果就是第一个运算数；
+    //若第一个运算数为null，运算结果就是第二个运算数。
+    var len2 = b?.length ?: -1
+    println(len2)
+}
