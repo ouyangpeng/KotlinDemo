1ca8f689b7bd0db504dc922eab3936f88da1f01f
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 16:54:10 +0800

    1、Kotlin 字符串模板

---
+++ src/kotlinTest/variable/StringTemplate.kt
@@ -0,0 +1,25 @@
+package kotlinTest.variable
+
+fun main(args: Array<String>) {
+    // 在字符串中 嵌入 变量
+    var bookPrice = 89
+    var s = "价格是：${bookPrice}"
+    println(s)
+
+    // 在字符串中 嵌入 方法调用
+    var random = java.util.Random()
+    var s2 = "伪随机数是：${random.nextInt(100)}"
+    println(s2)
+
+    var blog = "https://blog.csdn.net/ouyang_peng/"
+    println("${blog}的长度是：${blog.length}")
+
+    var QQ = "3024665621"
+    var QQGroup = "123133153"
+    var txt = """
+        |☞ 本人QQ: ${QQ}
+        |☞ QQ交流群:  ${QQGroup}
+        |☞ 博客地址${blog}
+    """.trimMargin()
+    println(txt)
+}
