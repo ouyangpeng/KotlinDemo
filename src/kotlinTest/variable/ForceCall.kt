1b221b8fbd383a5bba444f0aaff896329f937e5a
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 14:49:42 +0800

    1、Kotlin 强制调用 !!.

---
+++ src/kotlinTest/variable/ForceCall.kt
@@ -0,0 +1,22 @@
+package kotlinTest.variable
+
+fun main(args: Array<String>) {
+    //安全调用 ?.
+    //强制调用 !!.
+
+    //用 !! 即可强制调用可空变量的方法或属性 这样强制调用可能引发NPE
+    var b: String? = "oyp"
+    println(b!!.length)  // 3
+    b = null
+    println(b!!.length)  // 引发 kotlin.KotlinNullPointerException 空指针异常
+
+
+    var myArr: Array<String?> = arrayOf("oyp", null, "csdn", "xtc")
+    for (item in myArr) {
+        // 只有当item元素不为null才会执行的let函数
+        item!!.let { println(it) }
+        //oyp
+        //csdn
+        //xtc
+    }
+}
