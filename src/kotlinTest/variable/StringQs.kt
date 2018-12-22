6bbfe933d3e06a3b039dfdfac6b5bfb9a90d4656
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 16:39:15 +0800

    1、Kotlin 字符串

---
+++ src/kotlinTest/variable/StringQs.kt
@@ -0,0 +1,12 @@
+package kotlinTest.variable
+
+fun main(args: Array<String>) {
+    var str = "https://blog.csdn.net/ouyang_peng/"
+    println(str[2])
+    println(str[5])
+    println(str[10])
+    //循环遍历str字符串中的每个字符
+    for (c in str){
+        println(c)
+    }
+}
