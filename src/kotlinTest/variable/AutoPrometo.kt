d9df6449170d29921fc454f51b7b61e312dbbe82
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 11:28:06 +0800

    1、Kotlin   表达式 中包含 字符串和+号的两种情形

--- src/kotlinTest/variable/AutoPrometo.kt
+++ src/kotlinTest/variable/AutoPrometo.kt
@@ -27,8 +27,17 @@ fun main(args: Array<String>) {
     // 表达式的类型将严格保持和表达式中最高等级操作数相同的类型
     // 右边表达式两个操作数都是Int类型，因此右边表达式类型为Int
     // 虽然 23/3 不能除尽  但是依然得到一个Int类型整数
-    val intVar : Int = 3
+    val intVar: Int = 3
     val intResult = 23 / intVar
     println("intResult = $intResult")                         //intResult = 7
     println("intResult的类型为 = ${intResult.javaClass}")     //intResult的类型为 = int
+
+
+    // 1、先进行 "Hello!" + 'a' 运算，把'a'转换成字符串，拼接成    Hello!a
+    // 2、然后再进行 Hello!a + 7运算  也是字符串连接运算,拼接成    Hello!a7
+    println("Hello!" + 'a' + 7)  //Hello!a7
+
+    // 1、先进行 'a'  + 7 运算，此时是对Char型值做加法运算，将会得到 'a'后面的第7个字符（字符编码加7），因此得到 'h'
+    // 2、然后再进行 'h' + "Hello!" 运算，此时把'h'转换成字符串,字符串连接运算,拼接成  hHello!
+    println('a' + 7 + "Hello!")  //hHello!
 }
