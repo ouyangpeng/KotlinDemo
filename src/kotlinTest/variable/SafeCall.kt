fdd38237a610907d8e7e9ad3aaa6cfe79baab51b
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-22 14:36:37 +0800

    1、Kotlin 安全调用，不导致NPE

---
+++ src/kotlinTest/variable/SafeCall.kt
@@ -0,0 +1,20 @@
+package kotlinTest.variable
+
+fun main(args: Array<String>) {
+
+    // 安全调用  如果 b 不等于null，则返回b的长度，如果b为null，则返回null，不会导致NPE
+    var b: String? = "oyp"
+    println(b?.length)  // 3
+    b = null
+    println(b?.length)  // null
+
+
+    var myArr: Array<String?> = arrayOf("oyp", null, "csdn", "xtc")
+    for (item in myArr) {
+        // 只有当item元素不为null才会执行的let函数
+        item?.let { println(it) }
+        //oyp
+        //csdn
+        //xtc
+    }
+}
